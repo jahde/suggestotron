d2ab8985a73c04ac555daf2b0d663c33db41397f
Jahde <jahde.hiywan@gmail.com>
2014-09-27 14:07:41 -0400

    Added and linked the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Suggestotron::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  match '/help',    to:  'static_pages#help',     via: 'get'
+  match '/about',   to:  'static_pages#about',    via: 'get'
+  match '/contact', to:  'static_pages#contact',  via: 'get'   
+  
   resources :topics do
     member do
       post 'upvote'
